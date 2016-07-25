6e1c14c523b61b537789b0a2bc5c5fee277e59c7
puppy <puppy955101@gmail.com>
2016-07-25 21:43:45 +0800

    final

---
+++ index.js
@@ -0,0 +1,6 @@
+var But = document.querySelector('.input');
+var output = document.querySelector('.output');
+But.addEventListener('click',
+	function (e) {
+	document.body.style.backgroundColor = '#' + Math.random().toString(16).slice(2,8);
+});
