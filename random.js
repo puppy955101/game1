9ae9af65f5974d69200a27813c5e43c79ec95a34
puppy955101 <puppy955101@gmail.com>
2016-08-13 22:46:02 +0800

    final

--- random.js
+++ random.js
@@ -1,2 +1,3 @@
 document.body.style.backgroundColor = '#' + Math.random().toString(16).slice(2,8);
+
 document.querySelector('.input').style.backgroundColor = '#' + Math.random().toString(16).slice(2,8);
